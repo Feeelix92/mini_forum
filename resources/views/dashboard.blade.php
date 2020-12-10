a8375e47e1df43ac990afe888efff29a651ceabd
Felix Maurer <felix.maurer@informatik.hs-fulda.de>
2020-12-10 10:56:05 +0100

    Dashboard Layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">
-            Dashboard
+    <div class="flex h-screen grid grid-cols-3 text-center">
+        <div class="bg-blue-100 col-span-3 p-4 m-4">
+            <h1 class="text-xl mb-4">Willkommen!</h1>
+            <p class="mb-4">Auf dieser Seite können Sie Kommentare verfassen</p>
         </div>
-    </div>
+        <div class="bg-yellow-400 col-span-3 p-4 m-4">
+            <h1 class="text-xl mb-4">Bild!</h1>
+        </div>
+        <div class="bg-gray-400 p-4 m-4">
+            <p>
+                1. Beliebtester Kommentar
+            </p>
+        </div>
+        <div class="bg-green-400 p-4 m-4">
+            <p>
+                2. Beliebtester Kommentar
+            </p>
+        </div>
+        <div class="bg-pink-400 p-4 m-4">
+            <p>
+                3. Beliebtester Kommentar
+            </p>
+        </div>
+        <div class="bg-purple-400 p-4 m-4">
+            <p>
+                4. Beliebtester Kommentar
+            </p>
+        </div>
+        <div class="bg-blue-400 p-4 m-4">
+            <p>
+                5. Beliebtester Kommentar
+            </p>
+        </div>
+        <div class="bg-indigo-400 p-4 m-4">
+            <p>
+                6. Beliebtester Kommentar
+            </p>
+        </div>
+    <div>
 @endsection
